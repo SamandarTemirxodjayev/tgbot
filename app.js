a698cc8a862e024d7d392baf9e5637945b96deb5
SamandarTemirxodjayev <82377856+SamandarTemirxodjayev@users.noreply.github.com>
2021-04-12 17:03:18 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const token = '1680049120:AAH0IY83YSdLJguqUN4zfN46O5WN9Y2YjbA'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(token, {polling: true})
 
-bot.on('message', msg => {
+bot.on('text', async (message) => {
     if (message.text == '/start') {
-        return message.send('hello world');
+        message.send = (text, params) => bot.sendMessage(message.chat.id, text, params);
+        return message.send('hello world',{});
     }
 })
