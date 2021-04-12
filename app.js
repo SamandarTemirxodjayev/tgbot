b925f443fc16c56ba9f0fe7c95eafc50edd088cf
SamandarTemirxodjayev <82377856+SamandarTemirxodjayev@users.noreply.github.com>
2021-04-12 17:14:02 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const bot = new TelegramBot(token, {polling: true})
 bot.on('text', async (message) => {
     if (message.text == '/start') {
         message.send = (text, params) => bot.sendMessage(message.chat.id, text, params);
-        return message.send('hello world',{});
+        return message.send(`✌️ <b>Привет, ${message.from.first_name}</b>
+📝 <b>Цель игры:</b>
+├─Пополняем счет 🤘
+├─Покупаем команды 🏆
+├─Собираем Голы 🎖
+├─Обмениваем Голы 💵
+└─Получаем деньги 💹
+
+❓ Чем мы выплачиваем?
+❗️ С пополнения бота, продажи рекламы нашего проекта.
+💳 <b>Выплаты</b> 👉 @paymentsfootball`,{
+    reply_markup: {
+        keyboard: [
+            [{ text: "🛒 Магазин" }],
+            [{ text: "🏆Мои Команды" }]
+        ]
+    },
+    resize_keyboard: true
+});
     }
 })
